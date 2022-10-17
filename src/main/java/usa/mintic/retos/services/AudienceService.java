dfe77de2e7a9f934425ca965398f6d7086a801f4
John-Peña <jcandil1999@gmail.com>
2022-10-17 12:21:38 -0500

    corregido

---
+++ src/main/java/usa/mintic/retos/services/AudienceService.java
@@ -0,0 +1,64 @@
+package usa.mintic.retos.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import usa.mintic.retos.entity.Audience;
+import usa.mintic.retos.repository.AudienceRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AudienceService {
+
+    @Autowired
+    private AudienceRepository audienceRepository;
+
+    public List<Audience> getAll(){
+        return audienceRepository.getAll();
+    }
+
+    public Optional<Audience>getById(int id){
+        return audienceRepository.getById(id);
+    }
+
+    public Audience save(Audience c){
+        if(c.getId()==null){
+            return audienceRepository.save(c);
+        }
+        return c;
+    }
+    public boolean delete(int id){
+        Optional<Audience> cOp= audienceRepository.getById(id);
+        if(cOp.isPresent()){
+            audienceRepository.delete(cOp.get());
+            return true;
+        }
+        return false;
+    }
+    public Audience update(Audience c){
+        if(c.getId()!=null){
+            Optional<Audience> old= audienceRepository.getById(c.getId());
+            if(old.isPresent()){
+                Audience k=old.get();
+                if(c.getName()!=null){
+                    k.setName(c.getName());
+                }
+                if(c.getDescription()!=null){
+                    k.setDescription(c.getDescription());
+                }
+                if(c.getOwner()!=null){
+                    k.setOwner(c.getOwner());
+                }
+                if(c.getCapacity()!=null){
+                    k.setCapacity(c.getCapacity());
+                }
+                if(c.getCategory()!=null){
+                    k.setCategory(c.getCategory());
+                }
+                return audienceRepository.save(k);
+            }
+        }
+        return c;
+    }
+}
